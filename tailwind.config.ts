dcbabdc6f213ee17e30e3608b5a4a50b165c0c85
yerin <dpfls1022@naver.com>
2025-05-11 00:48:36 +0900

    chore(SCRUM-57): tailwind 텍스트 스타일 폰트 설정 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+// prettier-ignore
 import type { Config } from 'tailwindcss';
 import scrollbarHide from 'tailwind-scrollbar-hide';
 
@@ -13,10 +14,22 @@ const config: Config = {
         pretendard: ['Pretendard', 'sans-serif'],
       },
       colors: {
-        'main-color': '#65CE58',
+        'main-color': '#65CE58', // 이미 추가된 색상
       },
       boxShadow: {
-        'custom': '0px 4px 8px 0px rgba(0, 0, 0, 0.10)',
+        custom: '0px 4px 8px rgba(0, 0, 0, 0.10)',
+      },
+      fontSize: {
+        'headline-sb-main'    : ['20px', { lineHeight: '32px', letterSpacing: '0' }],
+        'title-sb-button'     : ['16px', { lineHeight: '20px', letterSpacing: '0' }],
+        'body-md-title'       : ['14px', { lineHeight: '18px', letterSpacing: '0' }],
+        'body-md-description' : ['12px', { lineHeight: '16px', letterSpacing: '0.1px' }],
+        'body-bd-title'       : ['16px', { lineHeight: '24px', letterSpacing: '0.1px' }],
+      },      
+      fontWeight: {
+        medium: '500',
+        semibold: '600',
+        bold: '700',
       },
     },
   },
