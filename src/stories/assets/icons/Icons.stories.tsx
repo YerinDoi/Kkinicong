8797138899e5118c782736f5f62f83e84477a977
soyeong <mb535622@sookmyung.ac.kr>
2025-05-13 02:10:10 +0900

    feat(SCRUM-49): GPS 버튼 추가

--- src/stories/assets/icons/Icons.stories.tsx
+++ src/stories/assets/icons/Icons.stories.tsx
@@ -1,27 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-
-import BackwardIcon from './system/backward.svg?react';
-import MenubarIcon from './system/menubar.svg?react';
-import LocationDropdownIcon from './system/location-dropdown.svg?react';
-import SearchIcon from './system/search.svg?react';
-import AllIcon from './menu/all.svg?react';
-import SnackIcon from './menu/snack.svg?react';
-import WesternIcon from './menu/western.svg?react';
-import ChineseIcon from './menu/chinese.svg?react';
-import ChickenIcon from './menu/chicken.svg?react';
-import HotpotIcon from './menu/hotpot.svg?react';
-import AsianIcon from './menu/asian.svg?react';
-import JapaneseIcon from './menu/japanese.svg?react';
-import KoreanIcon from './menu/korean.svg?react';
-import StreetFoodIcon from './menu/street-food.svg?react';
-import LunchBoxIcon from './menu/lunch-box.svg?react';
-import EtcIcon from './menu/etc.svg?react';
-import DropdownIcon from './system/dropdown.svg?react';
-import DropupIcon from './system/dropup.svg?react';
-import StarIcon from './system/star.svg?react';
-import HeartIcon from './system/heart.svg?react';
-import HeartFilledIcon from './system/heart-filled.svg?react';
 import Icon from '@/assets/icons';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
@@ -169,5 +147,11 @@ export const Heart: Story = {
 export const HeartFilled: Story = {
   args: {
     name: 'heart-filled',
+  },
+};
+
+export const Gps: Story = {
+  args: {
+    name: 'gps',
   },
 };
