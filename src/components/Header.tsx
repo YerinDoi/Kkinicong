1ebd18d77a9b7122619b5b69a74b3a33783aee4d
soyeong <mb535622@sookmyung.ac.kr>
2025-05-13 20:23:46 +0900

    feat(SCRUM-64): 메뉴바 UI 및 기능 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import Icon from '../assets/icons';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import MenuBar from './common/MenuBar';
 
 interface HeaderProps {
   title: string;
@@ -15,41 +17,56 @@ const Header = ({
   showMenubarButton = true,
 }: HeaderProps) => {
   const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(true);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
 
   return (
-    <div className="flex items-center w-full pt-[8px] pb-[8px] px-[20px]">
-      <button
-        className="flex items-center pt-[8px] pb-[8px] pr-[12px]"
-        onClick={() => navigate('/')}
-      >
-        <Icon name="backward" />
-      </button>
+    <>
+      <div className="flex items-center w-full pt-[8px] pb-[8px] px-[20px]">
+        <button
+          className="flex items-center pt-[8px] pb-[8px] pr-[12px]"
+          onClick={() => navigate('/')}
+        >
+          <Icon name="backward" />
+        </button>
 
-      <div className="flex items-end gap-[8px] ml-[4px]">
-        <span className="text-black text-[20px] font-semibold leading-[32px]">
-          {title}
-        </span>
+        <div className="flex items-end gap-[8px] ml-[4px]">
+          <span className="text-black text-[20px] font-semibold leading-[32px]">
+            {title}
+          </span>
 
-        {location && (
-          <div className="flex items-center gap-[4px] mb-[2px]">
-            <span className="text-[#919191] font-pretendard text-sm font-medium leading-[18px]">
-              {location}
-            </span>
-            {showLocationDropdown && (
-              <button>
-                <Icon name="location-dropdown" />
-              </button>
-            )}
-          </div>
+          {location && (
+            <div className="flex items-center gap-[4px] mb-[2px]">
+              <span className="text-[#919191] font-pretendard text-sm font-medium leading-[18px]">
+                {location}
+              </span>
+              {showLocationDropdown && (
+                <button>
+                  <Icon name="location-dropdown" />
+                </button>
+              )}
+            </div>
+          )}
+        </div>
+
+        {showMenubarButton && (
+          <button
+            className="w-[24px] h-[24px] aspect-square ml-auto"
+            onClick={toggleMenu}
+          >
+            <Icon name="menubar" />
+          </button>
         )}
       </div>
-
-      {showMenubarButton && (
-        <button className="w-[24px] h-[24px] aspect-square ml-auto">
-          <Icon name="menubar" />
-        </button>
-      )}
-    </div>
+      <MenuBar isOpen={isMenuOpen} onClose={closeMenu} />
+    </>
   );
 };
 
