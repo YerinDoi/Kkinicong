ed2428012ad10a403cc6665ec12a0704152761ca
chaeyeonni <126552675+intelsally@users.noreply.github.com>
2025-06-24 17:05:20 +0900

    style(SCRUM-195):오늘 끼니는 어때요 카드 행간 등 UI 수정

--- src/components/home/TopStoreItem.tsx
+++ src/components/home/TopStoreItem.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from 'react-router-dom';
 import Icon, { IconName } from '@/assets/icons';
-import axiosInstance from '@/api/axiosInstance';
 import { categoryIconMap } from '@/constants/categories';
 import { Store } from '@/types/store';
 
@@ -20,21 +19,21 @@ const TopStoreItem = ({ store }: TopStoreItemProps) => {
       className="p-[12px] bg-[#FEFEFE] z-50 shadow-custom rounded-[10px] w-[124px] h-[144px] "
       onClick={handleStoreClick}
     >
-      <div className="self-stretch flex flex-col justify-between items-start ">
+      <div className="self-stretch flex flex-col justify-between items-start gap-[6px]">
         <Icon
           name={categoryIconMap[store.category] || 'etc'}
           className="max-w-[42px] max-h-[42px] w-auto h-auto object-contain"
         />
         <div className="self-stretch flex flex-col justify-start items-start gap-[4px]">
-          <div className="justify-start text-black text-base font-bold font-['Pretendard'] tracking-tight">
+          <div className="justify-start leading-[1.1875] text-black text-base font-bold font-['Pretendard'] tracking-tight">
             {store.name}
           </div>
-          <div className="justify-start text-[#919191] text-xs font-normal font-['Pretendard'] leading-none tracking-tight">
+          <div className="justify-start leading-[150%] text-[#919191] text-xs font-normal font-['Pretendard'] leading-none tracking-tight">
             {store.address}
           </div>
         </div>
       </div>
-      <div className="self-stretch justify-start text-[#919191] text-xs font-normal font-['Pretendard'] leading-none tracking-tight">
+      <div className="self-stretch leading-[150%] justify-start text-[#919191] text-xs font-normal font-['Pretendard'] leading-none tracking-tight">
         조회 {store.viewCount}
       </div>
     </div>
