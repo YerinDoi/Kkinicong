6d54b8d13ca02399d89858f71783ef8138cb8725
chaeyeonni <126552675+intelsally@users.noreply.github.com>
2025-06-27 20:51:28 +0900

    feat(SCRUM-213):커뮤니티 카테고리 드롭다운 추가

--- src/pages/community/CommunityPage.tsx
+++ src/pages/community/CommunityPage.tsx
@@ -3,11 +3,12 @@ import { useNavigate } from "react-router-dom";
 import AlarmIcon from '@/assets/icons/system/alarm.svg';
 import SearchIcon from '@/assets/icons/system/search-black.svg';
 import PopularPosts from "@/components/Community/PopularPosts";
+import Dropdown from "@/components/common/Dropdown";
 
 const CommunityPage = () => {
   const navigate = useNavigate();
   return(
-    <div className="px-[20px] flex flex-col">
+    <div className="flex flex-col">
       <TopBar title="커뮤니티"
               paddingX="px-[15px]"
               rightType="custom"
@@ -22,6 +23,14 @@ const CommunityPage = () => {
                 />
               </div> }/>
       <PopularPosts/>
+      <div className="flex justify-end pr-[20px]">
+        <Dropdown
+        options={['전체', '복지정보', '잡담해요', '양육/육아', '문의/도움', '생활꿀팁','칭찬/감사','기타']}
+        onSelect={(val) => console.log('선택한 카테고리:', val)} // 이거 수정예정
+      />
+
+      </div>
+      
 
     </div>
     
