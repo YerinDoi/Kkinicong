6602f65989fc8c9883bda85cbc3fb40bb557d9b7
chaeyeonni <126552675+intelsally@users.noreply.github.com>
2025-06-28 19:13:48 +0900

    feat(SCRUM-195): 글쓰기 버튼 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,7 @@ const config: Config = {
       boxShadow: {
         custom: '2px 4px 10px 0px rgba(0, 0, 0, 0.20)',
         bottom: '0px 4px 8px 0px rgba(0, 0, 0, 0.10)',
+        floating: '0px 0px 8px 0px rgba(0, 0, 0, 0.25)'
       },
       fontSize: {
         'headline-sb-main'    : ['20px', { lineHeight: '32px', letterSpacing: '0' }],
