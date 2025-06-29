509e0adc72a5feade2ac889953eee2df252192a0
chaeyeonni <126552675+intelsally@users.noreply.github.com>
2025-06-29 18:32:51 +0900

    feat(SCRUM-226):커뮤니티 글 목록 페이지 레이아웃 수정

--- src/components/Community/PostItem.tsx
+++ src/components/Community/PostItem.tsx
@@ -22,15 +22,26 @@ const PostItem = ({ post }: { post: Post }) => {
     <div className="flex flex-col px-[20px] gap-[12px] py-[12px] border-b border-[#C3C3C3]">
     
       <div className={`flex ${hasThumbnail ? 'flex-row justify-between ' : 'flex-col gap-[12px]'} `}> 
-        <MainTag rounded="rounded-[8px]" text={post.category} />
-        <div className="flex flex-col gap-[8px]">
-          <h3 className="font-semibold text-title-sb-button">
-            {post.titlePreview.length > 25 ? post.titlePreview.slice(0, 25) + '…' : post.titlePreview}
-          </h3>
-          <p className="text-body-md-title text-[#C3C3C3]">
-            {post.contentPreview.length > 50 ? post.contentPreview.slice(0, 50) + '…' : post.contentPreview}
-          </p>
+        <div
+          className={`flex flex-col ${
+            hasThumbnail ? 'gap-[8px]' : 'gap-[12px]'
+          }`}
+        >
+          <MainTag rounded="rounded-[8px]" text={post.category} />
+          <div className="flex flex-col gap-[8px]">
+            <h3 className="font-semibold text-title-sb-button">
+              {post.titlePreview.length > 25
+                ? post.titlePreview.slice(0, 25) + '…'
+                : post.titlePreview}
+            </h3>
+            <p className="text-body-md-title text-[#C3C3C3]">
+              {post.contentPreview.length > 50
+                ? post.contentPreview.slice(0, 50) + '…'
+                : post.contentPreview}
+            </p>
+          </div>
         </div>
+
 
          {hasThumbnail && (
           <div className="relative w-[88px] h-[88px] rounded-[8px] overflow-hidden shrink-0">
@@ -40,7 +51,7 @@ const PostItem = ({ post }: { post: Post }) => {
               className="w-full h-full object-cover"
             />
             {(post.imageCount ?? 0) > 0  && (
-              <div className="absolute top-[4px] left-[4px] w-[29.3px] h-[29.3px] bg-[rgba(68,60,54,0.94)] text-white text-[12px] px-[7px] py-[2px] rounded-[8px]">
+              <div className="flex justify-center absolute top-0 left-0 w-[29.3px] h-[29.3px] bg-[rgba(68,60,54,0.94)] items-center text-center  text-white text-[12px] px-[7px] py-[2px] rounded-[8px]">
                 {post.imageCount}
               </div>
             )}
