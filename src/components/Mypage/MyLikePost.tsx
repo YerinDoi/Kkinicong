4dd60f95111691493b819ceb8afee65beef86e14
soyeong <mb535622@sookmyung.ac.kr>
2025-07-05 19:30:33 +0900

    feat(SCRUM-292): 좋아요 페이지 UI 및 기능 구현

--- src/components/Mypage/MyLikePost.tsx
+++ src/components/Mypage/MyLikePost.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import CommentIcon from '@/assets/svgs/common/comment.svg';
+import { useNavigate } from 'react-router-dom';
 
 interface LikePost {
   communityPostId: number;
-  category: string;
   titlePreview: string;
   contentPreview: string;
   thumbnailUrl: string | null;
@@ -19,25 +20,35 @@ interface MyLikePostProps {
 }
 
 const MyLikePost: React.FC<MyLikePostProps> = ({ like }) => {
+  const navigate = useNavigate();
   return (
-    <div className="px-[16px] py-[12px] border-b-[1.5px] border-[#E6E6E6]">
+    <div
+      className="flex flex-col gap-[12px] px-[20px] py-[19px] self-stretch border-b-[1.5px] border-[#E6E6E6] cursor-pointer"
+      onClick={() => navigate(`/community/post/${like.communityPostId}`)}
+    >
       <div className="flex flex-col gap-[8px]">
-        <div className="flex items-center gap-[8px]">
-          <span className="text-[12px] text-[#919191] bg-[#F4F6F8] px-[8px] py-[2px] rounded-[4px]">
-            {like.category}
-          </span>
-          <span className="text-[12px] text-[#919191]">{like.createdAt}</span>
-        </div>
-        <h3 className="font-medium text-[16px] text-[#1A1A1A]">
+        <span className="font-pretendard text-title-sb-button font-medium text-[16px] text-[#212121]">
           {like.titlePreview}
-        </h3>
-        <p className="text-[14px] text-[#616161] line-clamp-2">
+        </span>
+        <span className="font-pretendard text-body-md-title font-normal text-[#C3C3C3]">
           {like.contentPreview}
-        </p>
-        <div className="flex items-center gap-[16px] text-[12px] text-[#919191]">
-          <span>댓글 {like.commentCount}</span>
+        </span>
+      </div>
+
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-[5px] font-pretendard text-body-md-title font-normal text-[#C3C3C3]">
           <span>좋아요 {like.likeCount}</span>
+          <span>·</span>
+          <span>{like.createdAt}</span>
+          <span>·</span>
           <span>조회 {like.viewCount}</span>
+        </div>
+
+        <div className="flex items-center gap-[4px]">
+          <img src={CommentIcon} className="w-[15px] h-[14px]" />
+          <span className="font-pretendard text-[16px] font-normal text-[#C3C3C3] tracking-[0.016px]">
+            {like.commentCount}
+          </span>
         </div>
       </div>
     </div>
