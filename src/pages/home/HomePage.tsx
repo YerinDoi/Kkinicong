627a792d603c064d9424cf7f0419b9d6f7fe8d68
soyeong <mb535622@sookmyung.ac.kr>
2025-08-21 08:37:24 +0900

    chore(SCRUM-378): 메타 태그 정리

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,3 +1,5 @@
+import { Helmet } from 'react-helmet-async';
+
 import HeaderToCarouselSection from '@/components/home/HeaderToCarouselSection';
 import CategorySection from '@/components/home/CategorySection';
 import Top8StoreSection from '@/components/home/Top8StoreSection';
@@ -5,15 +7,35 @@ import ConvenienceStoreSection from '@/components/home/ConvenienceStoreSection';
 
 function HomePage() {
   return (
-    <div className="flex flex-col font-pretendard pb-[61px]">
-      <HeaderToCarouselSection />
+    <>
+      <Helmet>
+        {/* 브라우저 탭/검색결과 제목 */}
+        <title>끼니콩</title>
 
-      <div className="flex flex-col gap-[16px]">
-        <CategorySection />
-        <Top8StoreSection />
-        <ConvenienceStoreSection />
+        {/* 네이버 검색결과 설명 */}
+        <meta
+          name="description"
+          content="가까운 아동급식카드 가맹점 이곳에서 찾아봐요"
+        />
+
+        {/* Open Graph (카톡/네이버 블로그 등 공유 시 미리보기) */}
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="끼니콩" />
+        <meta property="og:description" content="가까운 아동급식카드 가맹점 이곳에서 찾아봐요" />
+        <meta property="og:url" content="https://kkinicong.co.kr" />
+        <meta property="og:image" content="https://kkinicong.co.kr/og-image.png" />
+      </Helmet>
+
+      <div className="flex flex-col font-pretendard pb-[61px]">
+        <HeaderToCarouselSection />
+
+        <div className="flex flex-col gap-[16px]">
+          <CategorySection />
+          <Top8StoreSection />
+          <ConvenienceStoreSection />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
