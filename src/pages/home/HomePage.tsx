c80f315e67b4766034d588f305c531151bf77607
soyeong <mb535622@sookmyung.ac.kr>
2025-08-21 15:58:23 +0900

    fix: 홈페이지 ; 수정

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -4,7 +4,6 @@ import HeaderToCarouselSection from '@/components/home/HeaderToCarouselSection';
 import CategorySection from '@/components/home/CategorySection';
 import Top8StoreSection from '@/components/home/Top8StoreSection';
 import ConvenienceStoreSection from '@/components/home/ConvenienceStoreSection';
-import CommunitySection from '@/components/home/CommunitySection';
 
 function HomePage() {
   return (
@@ -30,11 +29,11 @@ function HomePage() {
       <div className="flex flex-col font-pretendard pb-[61px]">
         <HeaderToCarouselSection />
 
-      <div className="flex flex-col gap-[16px]">
-        <CategorySection />
-        <Top8StoreSection />
-        <ConvenienceStoreSection />
-        <CommunitySection/>
+        <div className="flex flex-col gap-[16px]">
+          <CategorySection />
+          <Top8StoreSection />
+          <ConvenienceStoreSection />
+        </div>
       </div>
     </>
   );
