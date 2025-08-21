9e50a00e9fff61872af9f0336b19ccef38b4b8dd
soyeong <mb535622@sookmyung.ac.kr>
2025-08-21 16:34:20 +0900

    fix: import 추가

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -4,6 +4,7 @@ import HeaderToCarouselSection from '@/components/home/HeaderToCarouselSection';
 import CategorySection from '@/components/home/CategorySection';
 import Top8StoreSection from '@/components/home/Top8StoreSection';
 import ConvenienceStoreSection from '@/components/home/ConvenienceStoreSection';
+import CommunitySection from '@/components/home/CommunitySection';
 
 function HomePage() {
   return (
