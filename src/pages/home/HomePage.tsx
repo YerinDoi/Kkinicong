b6ebd665b8fc99bfa2865e98ab5a5e0b8ab9e6c5
chaeyeonni <126552675+intelsally@users.noreply.github.com>
2025-08-21 12:32:24 +0900

    style(SCRUM-385):메인페이지 배너/로그인하기 줄바꿈 수정

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -22,7 +22,10 @@ function HomePage() {
         {/* Open Graph (카톡/네이버 블로그 등 공유 시 미리보기) */}
         <meta property="og:type" content="website" />
         <meta property="og:title" content="끼니콩" />
-        <meta property="og:description" content="가까운 아동급식카드 가맹점 이곳에서 찾아봐요" />
+        <meta
+          property="og:description"
+          content="가까운 아동급식카드 가맹점 이곳에서 찾아봐요"
+        />
         <meta property="og:url" content="https://kkinicong.co.kr" />
         <meta property="og:image" content="https://kkinicong.co.kr/og-image.png" />
       </Helmet>
@@ -30,11 +33,12 @@ function HomePage() {
       <div className="flex flex-col font-pretendard pb-[61px]">
         <HeaderToCarouselSection />
 
-      <div className="flex flex-col gap-[16px]">
-        <CategorySection />
-        <Top8StoreSection />
-        <ConvenienceStoreSection />
-        <CommunitySection/>
+        <div className="flex flex-col gap-[16px]">
+          <CategorySection />
+          <Top8StoreSection />
+          <ConvenienceStoreSection />
+          <CommunitySection />
+        </div>
       </div>
     </>
   );
