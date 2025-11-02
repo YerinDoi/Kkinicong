2922aa3fbddfa4b160fbf7fd34f5bb554a6e758b
soyeong <mb535622@sookmyung.ac.kr>
2025-11-02 15:14:59 +0900

    feat(SCRUM-420): 다이얼로그 버튼 기능 구현

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,12 +1,19 @@
-
-
 import HeaderToCarouselSection from '@/components/home/HeaderToCarouselSection';
 import CategorySection from '@/components/home/CategorySection';
 import Top8StoreSection from '@/components/home/Top8StoreSection';
 import ConvenienceStoreSection from '@/components/home/ConvenienceStoreSection';
 import CommunitySection from '@/components/home/CommunitySection';
+import { useEffect, useState } from 'react';
+import RegionUpdateDialog from '@/components/common/RegionUpdateDialog';
 
 function HomePage() {
+  const [openDialog, setOpenDialog] = useState(false);
+
+  useEffect(() => {
+    const hidden = localStorage.getItem('hideRegionUpdateDialog');
+    if (hidden !== 'true') setOpenDialog(true);
+  }, []);
+
   return (
     <>
       <div className="flex flex-col font-pretendard pb-[61px]">
@@ -19,6 +26,14 @@ function HomePage() {
           <CommunitySection />
         </div>
       </div>
+      <RegionUpdateDialog
+        open={openDialog}
+        onGoHome={() => setOpenDialog(false)}
+        onDontShowAgain={() => {
+          localStorage.setItem('hideRegionUpdateDialog', 'true');
+          setOpenDialog(false);
+        }}
+      />
     </>
   );
 }
